47769227598662e60a82cfa7559ebbe0afa004c3
nikita <nmkazantsev@gmail.com>
2026-01-14 19:16:24 +0300

    working android launcher

---
+++ app/src/main/java/com/example/androiddemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.androiddemo;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nikitos.Engine;
+import com.nikitos.MainRenderer;
+import com.seal.gl_engine.AndroidLauncher;
+import com.seal.gl_engine.AndroidLauncherParams;
+
+public class MainActivity extends AppCompatActivity {
+    Engine engine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        window.getDecorView().setSystemUiVisibility(uiOptions);
+        AndroidLauncherParams androidLauncherParams = new AndroidLauncherParams(getApplicationContext())
+                .setDebug(true)
+                .setLandscape(true)
+                .setStartPage(unused -> new MainRenderer())
+                .setMSAA(true);
+
+        AndroidLauncher androidLauncher = new AndroidLauncher(androidLauncherParams);
+        engine = androidLauncher.getEngine();
+        setContentView(androidLauncher.launch());
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        engine.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        engine.onResume();
+    }
+}
